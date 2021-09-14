c0699e2ada02a79fad995136192fbda9c8d6ac31
Augusto Seabra <augusto.seabra00@gmail.com>
2021-09-13 23:24:14 -0300

    Adding first commit with tailwind and fontawesome settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Chat from './components/chat/Chat';
+import Contacts from './components/contacts/Contacts';
+import MkFramework from './components/mk-framework/MkFramework';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex">
+      <Contacts />
+      <Chat />
+      <MkFramework />
     </div>
   );
 }
